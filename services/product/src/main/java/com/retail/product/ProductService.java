ad465e7956dae3fd8ccb3f41571fec15c153666d
Danis <152644594+k-danis@users.noreply.github.com>
2026-07-17 03:27:46 +0600

    feat(product): implement CRUD by creating repository, service and exception

---
+++ services/product/src/main/java/com/retail/product/ProductService.java
@@ -0,0 +1,67 @@
+package com.retail.product;
+
+import com.retail.exception.ProductPurchaseException;
+import jakarta.persistence.EntityNotFoundException;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class ProductService {
+
+    private final ProductRepository repository;
+    private final ProductMapper mapper;
+
+    public Integer createProduct(@Valid ProductRequest request) {
+        var product = mapper.toProduct(request);
+        return repository.save(product).getId();
+    }
+
+    public List<ProductPurchaseResponse> purchaseProducts(@Valid List<ProductPurchaseRequest> request) {
+        var productIds = request.stream()
+                .map(ProductPurchaseRequest::productId)
+                .toList();
+
+        var storedProducts = repository.findAllByIdInOrderById(productIds);
+        if (productIds.size() != storedProducts.size()) {
+            throw new ProductPurchaseException("One or more products does not exist");
+        }
+
+        var storedRequest = request
+                .stream()
+                .sorted(Comparator.comparing(ProductPurchaseRequest::productId))
+                .toList();
+        var purchasedProducts = new ArrayList<ProductPurchaseResponse>();
+
+        for (int i = 0; i < storedRequest.size(); i++) {
+            var product = storedProducts.get(i);
+            var productRequest = storedRequest.get(i);
+            if (product.getAvailableQuantity() < productRequest.quantity())
+                throw new ProductPurchaseException("Insufficient quantity for product with ID : " + productRequest.productId());
+
+            var newAvailableQuantity = productRequest.quantity() - product.getAvailableQuantity();
+            product.setAvailableQuantity(newAvailableQuantity);
+            repository.save(product);
+            purchasedProducts.add(mapper.toProductPurchaseResponse(product, productRequest.quantity()));
+        }
+
+        return purchasedProducts;
+    }
+
+    public ProductResponse findById(Integer productId) {
+        return repository.findById(productId)
+                .map(mapper::toProductResponse)
+                .orElseThrow(() -> new EntityNotFoundException("Product not found with the ID : " + productId));
+    }
+
+    public List<ProductResponse> findAll() {
+        return repository.findAll().stream()
+                .map(mapper::toProductResponse)
+                .toList();
+    }
+}
