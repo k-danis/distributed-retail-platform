815d0b5a4f58c59643bbfea662b21682d2195539
Danis <152644594+k-danis@users.noreply.github.com>
2026-07-30 02:14:48 +0600

    feat(payment): implement payment creating logic

---
+++ services/payment/src/main/java/com/retail/payment/PaymentService.java
@@ -0,0 +1,33 @@
+package com.retail.payment;
+
+import com.retail.notification.NotificationProducer;
+import com.retail.notification.PaymentNotificationRequest;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class PaymentService {
+
+    private final PaymentRepository repository;
+    private final PaymentMapper mapper;
+    private final NotificationProducer notificationProducer;
+
+    public Integer createPayment(@Valid PaymentRequest request) {
+        var payment = repository.save(mapper.toPayment(request));
+
+        notificationProducer.sendNotification(
+                new PaymentNotificationRequest(
+                        request.orderReference(),
+                        request.amount(),
+                        request.paymentMethod(),
+                        request.customer().firstname(),
+                        request.customer().lastname(),
+                        request.customer().email()
+                )
+        );
+
+        return payment.getId();
+    }
+}
