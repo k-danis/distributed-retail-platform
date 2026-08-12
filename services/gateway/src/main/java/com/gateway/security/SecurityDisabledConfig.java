1ffec89dcc81b07083caff9fbffc41a81c27cc06
Danis <152644594+k-danis@users.noreply.github.com>
2026-08-12 19:41:13 +0600

    chore(gateway): add 'no-auth' profile to bypass security filter chain for testing

--- services/gateway/src/main/java/com/gateway/security/SecurityDisabledConfig.java
+++ services/gateway/src/main/java/com/gateway/security/SecurityDisabledConfig.java
@@ -3,25 +3,21 @@ package com.gateway.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Configuration
 @EnableWebFluxSecurity
-@Profile("!no-auth")
-public class SecurityConfig {
-
+@Profile("no-auth")
+public class SecurityDisabledConfig {
     @Bean
     public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
-        http
+        return http
                 .csrf(ServerHttpSecurity.CsrfSpec::disable)
-                .authorizeExchange(exchange -> exchange
-                        .pathMatchers("(/eureka/**)").permitAll()
-                        .anyExchange().permitAll()
+                .authorizeExchange(exchanges -> exchanges
+                                .anyExchange().permitAll()
                 )
-                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()));
-        return http.build();
+                .build();
     }
 }
